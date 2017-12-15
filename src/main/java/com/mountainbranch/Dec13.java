cf229d97dd5faeeb3e864d85363b7974c83dfbf4
Anders Berggren <anders.e.berggren@hotmail.com>
2017-12-15 23:07:25 +0100

    December 13th improved

--- src/main/java/com/mountainbranch/Dec13.java
+++ src/main/java/com/mountainbranch/Dec13.java
@@ -2,85 +2,59 @@ package com.mountainbranch;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Dec13 {
-	private Map<Integer, Scanner> scanners = new HashMap<Integer, Scanner>();
-	private int lastScannerLayer = 0;
+	private Map<Integer, Integer> scanners = new HashMap<Integer, Integer>();
 
 	public Dec13(String[] input) {
 		for (String line : input) {
 			String[] words = line.split(": ");
-			Integer layer = Integer.parseInt(words[0]);
-			int depthOfLayer = Integer.parseInt(words[1]);
-			scanners.put(layer, new Scanner(layer, depthOfLayer));
-			lastScannerLayer = Math.max(lastScannerLayer, layer);
+			scanners.put(Integer.parseInt(words[0]), Integer.parseInt(words[1]));
 		}
 	}
 	
+	/** Returns the total severity of a packet starting at time 0. */
 	public int getTotalSeverity() {
-		int totalSeverity = 0;
-		for (Integer i = 0; i <= lastScannerLayer; i++) {
-			// See if caught by scanner
-			Scanner scannerAtCurrentLayer = scanners.get(i);
-			if (scannerAtCurrentLayer != null && scannerAtCurrentLayer.tryToCatchPacket()) {
-				totalSeverity += scannerAtCurrentLayer.severity;
-			}
-			
-			// Move scanners
-			for (Scanner scanner : scanners.values()) {
-				scanner.move();
-			}
-		}
-		return totalSeverity;
+		return getTotalSeverity(0).totalSeverity;
 	}
-	
+
+	/** Returns the minimum delay necessary for a packet to pass without being caught. */
 	public int getDelayToPassWithoutBeingCaught() {
 		int delay = 0;
 		while (true) {
-			// For each scanner, check if the scanner would catch packet with this delay
-			boolean caught = false;
-			for (Scanner scanner : scanners.values()) {
-				int timeEnteringLayer = delay + scanner.layer;
-				if (timeEnteringLayer % ((scanner.depthOfLayer-1)*2) == 0) {
-					caught = true;
-					break;
-				}
-			}
-			if (caught) {
-				delay++;
-			} else {
+			if (!getTotalSeverity(delay).caught) {
 				return delay;
 			}
+			delay++;
 		}
 	}
 	
-	private class Scanner {
-		public final int layer;
-		public final int depthOfLayer;
-		public final int severity;
-		public int position = 0;
-		public int deltaStep = 1;
+	private Result getTotalSeverity(int startDelay) {
+		int totalSeverity = 0;
+		boolean caught = false;
 		
-		public Scanner(int layer, int depthOfLayer) {
-			this.layer = layer;
-			this.depthOfLayer = depthOfLayer;
-			severity = layer * depthOfLayer;
-		}
-		
-		public void move() {
-			if (position + deltaStep < 0 || position + deltaStep >= depthOfLayer) {
-				deltaStep *= -1;
+		// For each scanner, check if the scanner catches the packet. If so, add severity.
+		for (Entry<Integer, Integer> entry : scanners.entrySet()) {
+			Integer layer = entry.getKey();
+			Integer depth = entry.getValue();
+			int timeEnteringLayer = startDelay+layer;
+			if (timeEnteringLayer % ((depth-1)*2) == 0) {
+				totalSeverity += layer*depth;
+				caught = true;
 			}
-			position += deltaStep;
 		}
 		
-		public void reset() {
-			position = 0;
-			deltaStep = 1;
-		}
+		return new Result(totalSeverity, caught);
+	}
+	
+	private class Result {
+		public final int totalSeverity;
+		public final boolean caught;
 		
-		public boolean tryToCatchPacket() {
-			return position == 0;
+		public Result(int totalSeverity, boolean caught) {
+			this.totalSeverity = totalSeverity;
+			this.caught = caught;
 		}
 	}
 }
