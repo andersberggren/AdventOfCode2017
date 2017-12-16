1f983bc92e66bda3ba5015678d8ea4ef8ddbade6
Anders Berggren <anders.e.berggren@hotmail.com>
2017-12-16 16:31:23 +0100

    December 16th improved

--- src/main/java/com/mountainbranch/Dec16.java
+++ src/main/java/com/mountainbranch/Dec16.java
@@ -4,32 +4,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Dec16 {
-	private static final int NUMBER_OF_PROGRAMS = 16;
-	private char[] line = new char[NUMBER_OF_PROGRAMS];
-	
-	public Dec16() {
-		for (int i = 0; i < NUMBER_OF_PROGRAMS; i++) {
-			line[i] = (char) ('a' + i);
-		}
-	}
+	private String programs = "abcdefghijklmnop";
 	
 	public String dance(String input) {
-		for (String action : input.split(",")) {
-			switch (action.charAt(0)) {
+		for (String instruction : input.split(",")) {
+			char typeOfInstruction = instruction.charAt(0);
+			String arg = instruction.substring(1);
+			switch (typeOfInstruction) {
 			case 's':
-				spin(Integer.parseInt(action.substring(1)));
+				spin(Integer.parseInt(arg));
 				break;
 			case 'x':
-				String[] indices = action.substring(1).split("/");
+				String[] indices = arg.split("/");
 				exchange(Integer.parseInt(indices[0]), Integer.parseInt(indices[1]));
 				break;
 			case 'p':
-				String[] programs = action.substring(1).split("/");
+				String[] programs = arg.split("/");
 				partner(programs[0].charAt(0), programs[1].charAt(0));
 				break;
 			}
 		}
-		return lineToString();
+		return programs;
 	}
 	
 	public String billionDances(String input) {
@@ -40,7 +35,7 @@ public class Dec16 {
 		do {
 			dance(input);
 			numberOfDancesLeft--;
-		} while (permutations.add(lineToString()));
+		} while (permutations.add(programs));
 		
 		// The permutations repeat every x times, so we can subtract a multiple of that from
 		// the number of remaining dances since it would result in the same permutation.
@@ -49,45 +44,22 @@ public class Dec16 {
 			dance(input);
 			numberOfDancesLeft--;
 		}
-		return lineToString();
+		return programs;
 	}
 	
 	private void spin(int numberOfProgramsToTheFront) {
-		int indexInOldLine = NUMBER_OF_PROGRAMS - numberOfProgramsToTheFront;
-		char[] newLine = new char[NUMBER_OF_PROGRAMS];
-		for (int i = 0; i < NUMBER_OF_PROGRAMS; i++) {
-			newLine[i] = line[indexInOldLine++ % NUMBER_OF_PROGRAMS];
-		}
-		line = newLine;
+		int indexOfNewFirst = programs.length() - numberOfProgramsToTheFront;
+		programs = programs.substring(indexOfNewFirst) + programs.substring(0, indexOfNewFirst);
 	}
 	
 	private void exchange(int positionA, int positionB) {
-		char programAtPositionA = line[positionA];
-		line[positionA] = line[positionB];
-		line[positionB] = programAtPositionA;
+		partner(programs.charAt(positionA), programs.charAt(positionB));
 	}
 	
 	private void partner(char programA, char programB) {
-		int indexOfA = getIndexOfProgram(programA);
-		int indexOfB = getIndexOfProgram(programB);
-		line[indexOfA] = programB;
-		line[indexOfB] = programA;
-	}
-	
-	private int getIndexOfProgram(char program) {
-		for (int i = 0; i < NUMBER_OF_PROGRAMS; i++) {
-			if (line[i] == program) {
-				return i;
-			}
-		}
-		throw new RuntimeException("Couldn't find program " + program + " in line");
-	}
-	
-	private String lineToString() {
-		String order = "";
-		for (int i = 0; i < NUMBER_OF_PROGRAMS; i++) {
-			order += line[i];
-		}
-		return order;
+		char temp = '.';
+		programs = programs.replace(programA, temp);
+		programs = programs.replace(programB, programA);
+		programs = programs.replace(temp, programB);
 	}
 }
