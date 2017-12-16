60957c6551129ff4c1135ba8fe3524718a777f74
Anders Berggren <anders.e.berggren@hotmail.com>
2017-12-16 17:01:50 +0100

    December 2nd improved

--- src/main/java/com/mountainbranch/Dec2.java
+++ src/main/java/com/mountainbranch/Dec2.java
@@ -1,19 +1,15 @@
 package com.mountainbranch;
 
+import java.util.Arrays;
+
 public class Dec2 {
 	public int calcChecksum1(String[] input) {
 		// The checksum of a line is the difference between the largest and smallest number
 		LineChecksumCalculator lcc = new LineChecksumCalculator() {
 			@Override
-			public int calculateLineChecksum(int[] values) {
-				int min = 0;
-				int max = 0;
-				for (int i = 0; i < values.length; i++) {
-					int value = values[i];
-					min = Math.min(value, i==0 ? value : min);
-					max = Math.max(value, i==0 ? value : max);
-				}
-				return max - min;
+			public int calculateLineChecksum(Integer[] values) {
+				Arrays.sort(values);
+				return values[values.length-1] - values[0];
 			}
 		};
 		return calcChecksum(input, lcc);
@@ -23,15 +19,13 @@ public class Dec2 {
 		// The checksum of a line is the quotient of the only two evenly divisible numbers
 		LineChecksumCalculator lcc = new LineChecksumCalculator() {
 			@Override
-			public int calculateLineChecksum(int[] values) {
-				for (int i = 0; i < values.length; i++) {
-					for (int k = i+1; k < values.length; k++) {
-						int a = values[i];
-						int b = values[k];
-						if (a % b == 0)
-							return a / b;
-						else if (b % a == 0)
-							return b / a;
+			public int calculateLineChecksum(Integer[] values) {
+				Arrays.sort(values);
+				for (int a = 0; a < values.length; a++) {
+					for (int b = a+1; b < values.length; b++) {
+						if (values[b] % values[a] == 0) {
+							return values[b] / values[a];
+						}
 					}
 				}
 				throw new IllegalArgumentException("No two numbers are evenly divisible");
@@ -44,7 +38,7 @@ public class Dec2 {
 		int checksum = 0;
 		for (String line : input) {
 			String[] words = line.split("[\t ]+");
-			int[] values = new int[words.length];
+			Integer[] values = new Integer[words.length];
 			for (int i = 0; i < values.length; i++)
 				values[i] = Integer.parseInt(words[i]);
 			checksum += lcc.calculateLineChecksum(values);
@@ -53,6 +47,6 @@ public class Dec2 {
 	}
 	
 	private static interface LineChecksumCalculator {
-		public int calculateLineChecksum(int[] values);
+		public int calculateLineChecksum(Integer[] values);
 	}
 }
